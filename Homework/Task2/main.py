3fa98f835ed5eca1d9b9ffb352f213e4f5c76e10
Maou <waspofcrimea@yandex.ru>
2023-01-29 13:32:10 +0300

    Solved task2

---
+++ Homework/Task2/main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+# Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов. Сколько
+# журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов,
+# а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?
+# Пример:
+# 6 -> 1  4  1
+# 24 -> 4  16  4
+# 60 -> 10  40  10
+
+
+
+
+number = int(input("Введите число:"))
+if number%3 != 0:
+    print('Неверное значение')
+
+else:
+    print ( f'{int(number/6)} {int(number/3)*2} {int(number/6)}')
