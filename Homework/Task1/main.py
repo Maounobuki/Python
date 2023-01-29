9ab7cc48f03f19fea1532fa9acb572c0d76946c6
Maou <waspofcrimea@yandex.ru>
2023-01-29 13:10:34 +0300

    solved task1

---
+++ Homework/Task1/main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+# Задание 1 Найдите сумму цифр трехзначного числа.
+# Пример:
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+
+
+number = str(input("Введите число:"))
+summ = int(number[0])+ int(number[1]) +int(number[2])
+
+print(summ)
+
