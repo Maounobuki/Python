cfba824700fe7de92bb2982e5078fb574bed04fb
Maou <waspofcrimea@yandex.ru>
2023-01-29 15:20:43 +0300

    Added changes to tasks 1-3

--- Homework/Task3/main.py
+++ Homework/Task3/main.py
@@ -1,21 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет
+#Задание 3. Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет
 # с номером. Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр
 # равна сумме последних трех.
 # Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6.
